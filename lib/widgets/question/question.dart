3ac37415053a10007e79edd68c5021aed02334b1
Ben Calabrese <bencalabrese@google.com>
2018-02-15 18:40:11 -0800

    hook up material

--- lib/widgets/question/question.dart
+++ lib/widgets/question/question.dart
@@ -1,11 +1,24 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_button/material_button.dart';
+import 'package:kindergarten_trivia/models/answer/answer.dart';
 import 'package:kindergarten_trivia/models/question/question.dart';
 
 @Component(
-  selector: 'question',
-  templateUrl: 'question.html',
-)
+    selector: 'question',
+    templateUrl: 'question.html',
+    directives: const [
+      MaterialButtonComponent,
+      NgFor,
+    ])
 class QuestionComponent {
   @Input()
   Question question;
+
+  void handleAnswer(Answer answer) {
+    if (question.isCorrect(answer)) {
+      print('hooray!');
+    } else {
+      print('boo');
+    }
+  }
 }
