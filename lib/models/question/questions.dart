6d9a374cdfe87b6b45cb9ef91353f41d71762e4f
Ben Calabrese <bencalabrese@bencalabrese-macbookpro.roam.corp.google.com>
2018-02-19 20:53:25 -0800

    move images into static assets

--- lib/models/question/questions.dart
+++ lib/models/question/questions.dart
@@ -6,25 +6,25 @@ const questions = const [
       text: 'What is the word for the number 4?',
       possibleAnswers: const [const Answer('four'), const Answer('seven')],
       correctAnswer: const Answer('four'),
-      gifURL: 'https://media.giphy.com/media/aQYR1p8saOQla/giphy.gif'),
+      gifURL: 'assets/anna_frozen.gif'),
   const Question(
       text: 'Which one is a word?',
       possibleAnswers: const [const Answer('brg'), const Answer('can')],
       correctAnswer: const Answer('can'),
-      gifURL: 'https://media.giphy.com/media/kzwzTIbi7sBm8/giphy.gif'),
+      gifURL: 'assets/alligator.gif'),
   const Question(
       text: 'What is 5 + 3?',
       possibleAnswers: const [const Answer('8'), const Answer('10')],
       correctAnswer: const Answer('8'),
-      gifURL: 'https://media.giphy.com/media/Ta2eHM043vhVS/giphy.gif'),
+      gifURL: 'assets/joy_inside_out.gif'),
   const Question(
       text: 'What is 7 - 2?',
       possibleAnswers: const [const Answer('3'), const Answer('5')],
       correctAnswer: const Answer('5'),
-      gifURL: 'https://media.giphy.com/media/B1LM7LhY6q4FO/giphy.gif'),
+      gifURL: 'assets/charlotte_princess_frog.gif'),
   const Question(
       text: 'What do plants need to grow?',
       possibleAnswers: const [const Answer('air'), const Answer('soda')],
       correctAnswer: const Answer('air'),
-      gifURL: 'https://media.giphy.com/media/YQboundML4OI0/giphy.gif'),
+      gifURL: 'assets/squishy_monsters_u.gif'),
 ];
