99cb7d0b438e59fcf6fbb40eb850fc7344fd3280
Ben Calabrese <bencalabrese@bencalabrese-macbookpro.roam.corp.google.com>
2018-02-19 19:06:27 -0800

    Add basic game flow

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,4 +1,6 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_button/material_button.dart';
+import 'package:kindergarten_trivia/models/question/question.dart';
 import 'package:kindergarten_trivia/models/question/questions.dart';
 import 'package:kindergarten_trivia/widgets/question/question.dart';
 
@@ -6,9 +8,31 @@ import 'package:kindergarten_trivia/widgets/question/question.dart';
   selector: 'my-app',
   templateUrl: 'app_component.html',
   directives: const [
-    NgFor,
     QuestionComponent,
+    MaterialButtonComponent,
+    NgFor,
+    NgSwitch,
+    NgSwitchWhen,
   ],
-  exports: const [questions],
+  exports: const [GameState],
 )
-class AppComponent {}
+class AppComponent {
+  GameState gameState = GameState.unstarted;
+  int _currentQuestionIndex = 0;
+
+  Question get currentQuestion => questions[_currentQuestionIndex];
+
+  void handleCorrectAnswer() {
+    _currentQuestionIndex++;
+    if (_currentQuestionIndex == questions.length) {
+      gameState = GameState.finished;
+    }
+  }
+}
+
+enum GameState {
+  unstarted,
+  inProgress,
+  finished,
+  treasure,
+}
--- lib/widgets/question/question.dart
+++ lib/widgets/question/question.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'package:angular_components/material_button/material_button.dart';
 import 'package:kindergarten_trivia/models/answer/answer.dart';
@@ -14,12 +16,18 @@ import 'package:kindergarten_trivia/models/question/question.dart';
       NgFor,
     ])
 class QuestionComponent {
+  final _onCorrect = new StreamController<Null>.broadcast();
+
   @Input()
   Question question;
+
+  @Output()
+  Stream<Null> get onCorrect => _onCorrect.stream;
 
   void handleAnswer(Answer answer) {
     if (question.isCorrect(answer)) {
       print('hooray!');
+      _onCorrect.add(null);
     } else {
       print('boo');
     }
