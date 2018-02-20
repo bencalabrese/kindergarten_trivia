1f54b953e4000c1d1a0e108bf7313f3136b1115c
Ben Calabrese <bencalabrese@bencalabrese-macbookpro.roam.corp.google.com>
2018-02-19 19:42:31 -0800

    Add gif support

--- lib/models/question/question.dart
+++ lib/models/question/question.dart
@@ -5,11 +5,13 @@ class Question {
   final String text;
   final List<Answer> possibleAnswers;
   final Answer correctAnswer;
+  final String gifURL;
 
   const Question({
     @required this.text,
     @required this.possibleAnswers,
     @required this.correctAnswer,
+    @required this.gifURL,
   });
 
   bool isCorrect(Answer answer) => answer == correctAnswer;
--- lib/models/question/questions.dart
+++ lib/models/question/questions.dart
@@ -5,13 +5,16 @@ const questions = const [
   const Question(
       text: 'What is the word for the number 4?',
       possibleAnswers: const [const Answer('four'), const Answer('seven')],
-      correctAnswer: const Answer('four')),
+      correctAnswer: const Answer('four'),
+      gifURL: 'https://media.giphy.com/media/RQgzLsPYlzrBC/giphy.gif'),
   const Question(
       text: 'What color is the sky?',
       possibleAnswers: const [const Answer('red'), const Answer('blue')],
-      correctAnswer: const Answer('blue')),
+      correctAnswer: const Answer('blue'),
+      gifURL: 'https://media.giphy.com/media/RQgzLsPYlzrBC/giphy.gif'),
   const Question(
       text: 'What noise does a cow make?',
       possibleAnswers: const [const Answer('woof'), const Answer('moo')],
-      correctAnswer: const Answer('moo')),
+      correctAnswer: const Answer('moo'),
+      gifURL: 'https://media.giphy.com/media/RQgzLsPYlzrBC/giphy.gif'),
 ];
